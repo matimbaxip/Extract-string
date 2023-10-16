3dd085ef1eb9b65c1c3a367daa0a7379f0cf61e7
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-16 10:34:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extract_string
+{
+    internal class Program
+    {
+        static string ExtractString(string input)
+        {
+            int start = input.IndexOf("##");
+            int end = input.LastIndexOf("##");
+
+            if (start >= 0 && end >= 0 && start < end)
+            {
+                return input.Substring(start + 2, end - start - 2);
+            }
+
+            return ""; // Return an empty string if no valid substring found.
+        }
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine(ExtractString("##abc##def"));    // "abc"
+            Console.WriteLine(ExtractString("12####78"));        // ""
+            Console.WriteLine(ExtractString("gar##d#en"));       // ""
+            Console.WriteLine(ExtractString("++##--##++"));      // "--"
+
+        }
+    }
+}
